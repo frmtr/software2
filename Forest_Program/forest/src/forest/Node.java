ed2c094dbe846055aaf00e75fbaf4809d0f4d899
takumi <takumi@koikeryouji-no-MacBook-Pro.local>
2019-07-24 10:02:03 +0900

    koike prpgram finish

--- Forest_Program/forest/src/forest/Node.java
+++ Forest_Program/forest/src/forest/Node.java
@@ -8,24 +8,58 @@ import javax.swing.JFrame;
 
 public class Node extends JFrame{
 
+	/**
+	 * ノードの名前をStringのインスタンスとして束縛する
+	 */
 	private String nodeName;
+	/**
+	 * ノードの番号をIntegerのインスタンスとして束縛する
+	 */
 	private Integer nodeNumber;
 
-	ArrayList<Node> parents;
-	ArrayList<Node> children;
 	/**
-	 * 座標
+	 * ノードの親ノードをNodeリストのインスタンスとして束縛する
 	 */
-	Point point;
+	private ArrayList<Node> parents;
 
-	int textWidth;
+	/**
+	 * ノードの子ノードをNodeリストのインスタンスとして束縛する。
+	 */
+	private ArrayList<Node> children;
 
+	/**
+	 * model座標をPointのインスタンスとして束縛する。
+	 */
+	private Point point;
+	/*
+	 * view座標をPointのインスタンスとして束縛する。
+	 */
+	private Point viewPoint;
+
+	/**
+	 * ノードの名前を画面上で描画したときの長さをIntegerのインスタンスとして束縛する
+	 */
+	private int textWidth;
+
+	/**
+	 * ノードが探索されたかどうかをBooleanのインスタンスとして束縛する。
+	 */
+	private boolean isSearched;
+
+	/**
+	 * インスタンスを生成して応答する。
+	 * @param aInteger ノードの番号
+	 * @param aString ノードの名前
+	 * @param aPoint ノードの初期位置
+	 */
 	public Node(Integer aInteger,String aString,Point aPoint){
 		this.nodeName = aString;
 		this.nodeNumber = aInteger;
 		this.parents = new ArrayList<Node>();
 		this.children = new ArrayList<Node>();
 		this.point = aPoint;
+		this.viewPoint = aPoint;
+		this.isSearched = false;
 
 		//描画時の文字列の長さ
 		Font font1 = new Font("Arial",Font.PLAIN,12);
@@ -33,35 +67,106 @@ public class Node extends JFrame{
 
 	}
 
+	/**
+	 * ノードの名前を応答する
+	 * @return ノードの名前
+	 */
 	public String getName(){
 		return this.nodeName;
 	}
 
+	/**
+	 * ノード名前を設定する
+	 */
 	public void setName(String aString){
 		this.nodeName = aString;
 	}
 
+	/**
+	 * ノードの番号を応答する
+	 * @return ノードの番号
+	 */
 	public Integer getNumber(){
 		return this.nodeNumber;
 	}
 
+	/**
+	 * ノードの番号を設定する
+	 * @param aInteger
+	 */
 	public void setNumber(Integer aInteger){
 		this.nodeNumber = aInteger;
 	}
+
+	/**
+	 * 親ノードを応答する
+	 * @return 親ノードのリスト
+	 */
 	public ArrayList<Node> getParents(){
 		return this.parents;
 	}
+
+	/**
+	 * 子ノードを応答する
+	 * @return 子ノードのリスト
+	 */
 	public ArrayList<Node> getChildren(){
 		return this.children;
 	}
 
+	/**
+	 * ノードのModel座標を応答する
+	 * @return
+	 */
 	public Point getPoint(){
 		return this.point;
 	}
+
+	/**
+	 * ノードのModel座標を設定する
+	 * @param aPoint
+	 */
 	public void setPoint(Point aPoint){
 		this.point = aPoint;
 	}
+
+	/**
+	 * ノードのView座標を設定する。
+	 * @return
+	 */
+	public Point getViewPoint(){
+		return this.viewPoint;
+	}
+
+	/**
+	 * ノードのView座標を設定する
+	 * @param aPoint
+	 */
+	public void setViewPoint(Point aPoint){
+		this.viewPoint = aPoint;
+	}
+
+	/**
+	 * ノードの名前のテキストの長さを応答する
+	 * @return ノードの名前のテキストの長さ
+	 */
 	public int getTextWidth(){
 		return this.textWidth;
+	}
+
+	/**
+	 * ノードが探索済みかどうかを応答する
+	 * @return ノードが探索済みかどうか
+	 */
+	public boolean getIsSearched() {
+		return isSearched;
+	}
+
+	/**
+	 * ノードが探索済みかどうかを設定する。
+	 * @param isSearched ノードが探索済みかどうか
+	 */
+	public void setIsSearched(boolean isSearched) {
+		this.isSearched = isSearched;
 	}
 }
